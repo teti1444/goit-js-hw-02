4fe6afc9318562c40eabc703ab005bc5bce36df2
teti1444 <tanaandrejko537@gmail.com>
2026-09-21 14:21:33 +0200

    complete task 2

--- js/task-1.js
+++ js/task-1.js
@@ -3,8 +3,8 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits){
     const totalPrice = quantity * pricePerDroid
     if(totalPrice > customerCredits){
-        return `Insufficient funds!`
-    } else return `You ordered ${quantity} droids worth ${totalPrice} credits!`
+        return `Insufficient funds!`;
+    } else return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
